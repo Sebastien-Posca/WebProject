49a652c83292ec4143d3ac3e9abec7a45a0feca5
Quentin <quentin.moreau@etu.unice.fr>
2020-02-03 15:15:04 +0100

    Zip file upload

---
+++ Server/routes/submit.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const controller = require("../controllers/submit");
+const router = express.Router();
+
+router.get("/home", (req, res) => res.sendFile(__dirname + '/index.html'));
+router.post("/", controller.upload.single("myFile"), controller.fileUpload);
+
+module.exports = router;
