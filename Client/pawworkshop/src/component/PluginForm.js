1d788e7dc9bff8fc58f281f22b5c29f786e19dd1
ValentinAh-Kane <valentin.ah-kane@etu.univ-cotedazur.fr>
2020-02-13 11:38:28 +0100

    .

--- Client/pawworkshop/src/component/PluginForm.js
+++ Client/pawworkshop/src/component/PluginForm.js
@@ -92,12 +92,13 @@ const PluginForm = props => {
 
         props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
-                setUploading(true);
+                // setUploading(true);
                 console.log('Received values of form: ', values);
                 formData.append('name', values.name);
                 formData.append('version', values.version);
                 formData.append('description', values.description);
                 formData.append('thumbnail', values.thumbnail.file.thumbUrl);
+                formData.append('categorie', values.categorie);
                 formData.append('tags', values.tags);
                 reqwest({
                     url: 'http://192.168.43.68:3000/submit',
@@ -183,6 +184,7 @@ const PluginForm = props => {
                 <Form.Item label="Category">
                     {getFieldDecorator('category', {
                         rules: [{ required: true, message: 'Please select a category!' }]
+
                     })(
                         <Select defaultValue={categories[0]}>
                             {categories.map((item) => {
--- Client/pawworkshop/src/component/PluginSummary.js
+++ Client/pawworkshop/src/component/PluginSummary.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const PluginSummary = props => {
+    const id = useParams();
+    const selectedPlugin = useSelector((state) => state.pluginSelected)
+    console.log(selectedPlugin)
     return (
-        <div>
+        <>
+            <div className="pluginCard">
 
-        </div>
-    );
+            </div>
+
+            <section>
+                <div className="pluginDescription">
+                    <h1> Description </h1>
+                    <p>Description du plugin séléctionné</p>
+
+                </div>
+            </section>
+            <section className="commentSection">
+                <div></div>
+            </section>
+        </>
+    )
 };
 
 export default PluginSummary;
