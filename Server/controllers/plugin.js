84db7a9ead16ba369b73431672bbed36ce6c09c5
Sébastien Posca <sebastien.posca@etu.unice.fr>
2020-02-13 10:33:46 +0100

    update server

--- Server/controllers/plugin.js
+++ Server/controllers/plugin.js
@@ -1,17 +1,17 @@
 const Plugin = require('../models/plugin')
-const User = require('../models/user')
+var fs = require('fs');
+var path = require('path');
+
 
 exports.createPlugin = (user, path, name) => {
-    User.findOne({ name: new RegExp('^' + user + '$', "i") }).then((doc) => {
-        let plugin = new Plugin();
-        plugin.user = doc;
-        plugin.path = path;
-        plugin.name = name;
-        plugin.save((err, plugin) => {
-            if (err) console.log(err);
-            console.log(plugin);
-            ;
-        });
+    let plugin = new Plugin();
+    plugin.user = user;
+    plugin.path = path;
+    plugin.name = name;
+    plugin.save((err, plugin) => {
+        if (err) console.log(err);
+        console.log(plugin);
+        ;
     });
 }
 
@@ -21,6 +21,12 @@ exports.getPlugins = (req, res) => {
         var pluginMap = [];
 
         users.forEach(function (plugin) {
+            let thumbnail = fs.readFileSync(plugin.path + '/thumbnail.jpg', "utf8")
+            let pair = { thumbnail: thumbnail }
+
+            plugin = {
+                ...plugin._doc, ...pair
+            };
             pluginMap.push(plugin);
         });
 
@@ -31,5 +37,11 @@ exports.getPlugins = (req, res) => {
 exports.getPlugin = (req, res) => {
     Plugin.findById(req.params.id).then((doc) => {
         res.send(doc);
+    });
+}
+
+exports.getPlugin = (req, res) => {
+    Plugin.findById(req.params.id).then((doc) => {
+        res.sendFile(path.join(doc.path, doc.name, "index.html"));
     });
 }
