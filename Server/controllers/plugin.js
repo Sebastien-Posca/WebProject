b8e627b9f58dcba80296d8f289821ce2ee3b6901
Sébastien Posca <sebastien.posca@etu.unice.fr>
2020-02-17 15:13:19 +0100

    fix

--- Server/controllers/plugin.js
+++ Server/controllers/plugin.js
@@ -6,22 +6,22 @@ var express = require("express");
 
 
 exports.createPlugin = (user, path, name, moduleName, description, tags, category, version) => {
-  return new Promise((resolve, reject) => {
-    let plugin = new Plugin();
-    plugin.moduleName = moduleName;
-    plugin.user = user;
-    plugin.path = path;
-    plugin.name = name;
-    plugin.description = description;
-    console.log(tags);
-    plugin.tags = tags;
-    plugin.category = category;
-    plugin.version = version;
-    plugin.save((err, plugin) => {
-        if (err) reject(err);
-        resolve(plugin);
-    });
-  })
+    return new Promise((resolve, reject) => {
+        let plugin = new Plugin();
+        plugin.moduleName = moduleName;
+        plugin.user = user;
+        plugin.path = path;
+        plugin.name = name;
+        plugin.description = description;
+        console.log(tags);
+        plugin.tags = JSON.parse(tags);
+        plugin.category = category;
+        plugin.version = version;
+        plugin.save((err, plugin) => {
+            if (err) reject(err);
+            resolve(plugin);
+        });
+    })
 }
 
 
@@ -33,8 +33,13 @@ exports.getPlugins = (req, res) => {
 
         users.forEach(function (plugin) {
             let thumbnail = fs.readFileSync(plugin.path + '/thumbnail.jpg', "utf8")
-            let pair = { thumbnail: thumbnail }
+            let pair;
+            if (!thumbnail.startsWith('data:image')) {
+                pair = { thumbnail: "data:image/jpg;base64," + thumbnail }
 
+            } else {
+                pair = { thumbnail: thumbnail }
+            }
             plugin = {
                 ...plugin._doc, ...pair
             };
@@ -46,14 +51,14 @@ exports.getPlugins = (req, res) => {
 }
 
 exports.getPlugin = (req, res) => {
-    Plugin.findById(req.params.id).then((err, doc) => {
+    Plugin.findById(req.params.id).then((doc, err) => {
         if (err) return res.status(500).send(err);
         return res.status(200).send(doc);
     });
 }
 
 exports.servePlugin = (req, res) => {
-    Plugin.findById(req.params.id).then((err, doc) => {
+    Plugin.findById(req.params.id).then((doc, err) => {
         if (err) return res.status(500).send(err);
         express.static(path.join(__dirname, "../apps", doc.path))
         return res.sendFile(path.join(__dirname, "../apps", doc.path, req.params.filename));
