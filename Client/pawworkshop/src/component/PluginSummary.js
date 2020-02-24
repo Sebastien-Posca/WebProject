4c520f7845d618e1d1fb22825a129961acd4ef25
Lucas OMS <lucas.oms@etu.unice.fr>
2020-02-24 15:31:00 +0100

    CSS sur l'affichage du détail d'un plugin + tags

--- Client/pawworkshop/src/component/PluginSummary.js
+++ Client/pawworkshop/src/component/PluginSummary.js
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { Tabs, Tag, Avatar, Comment, Tooltip, Button, Icon, message } from 'antd';
-import { BACKEND_ROOT_PATH } from "../constants";
+import React, {useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
+import {useParams} from 'react-router-dom';
+import {Button, Icon, message, Tabs, Tag} from 'antd';
+import {BACKEND_ROOT_PATH} from "../constants";
 import Text from 'antd/lib/typography/Text';
-import moment from 'moment';
 import CommentComponent from './CommentComponent';
 import './PluginSummary.css';
 import reqwest from 'reqwest';
 
 
-const { TabPane } = Tabs;
+const {TabPane} = Tabs;
 const PluginSummary = props => {
     const params = useParams();
-    const selectedPlugin = useSelector((state) => state.pluginSelected)
+    const selectedPlugin = useSelector((state) => state.pluginSelected);
     const [fetchedPlugin, setFetchedPlugin] = useState(null);
     const [loading, setLoading] = useState(true);
     const [reload, setReload] = useState(0);
@@ -23,12 +22,12 @@ const PluginSummary = props => {
             const result = await fetch(`${BACKEND_ROOT_PATH}/plugins/${params.id}`);
             const results = await result.json();
 
-            console.log(result)
-            console.log(results)
+            console.log(result);
+            console.log(results);
 
             setFetchedPlugin(results);
             setLoading(false);
-        }
+        };
         fetchData();
     }, []);
 
@@ -39,7 +38,7 @@ const PluginSummary = props => {
             type: 'json',
             processData: false,
             contentType: 'application/json',
-            data: JSON.stringify({ id: fetchedPlugin._id }),
+            data: JSON.stringify({id: fetchedPlugin._id}),
             success: (plugin) => {
                 message.success('Like');
                 setFetchedPlugin(plugin);
@@ -49,7 +48,7 @@ const PluginSummary = props => {
 
             },
         });
-    }
+    };
 
     return (
         <>
@@ -57,28 +56,30 @@ const PluginSummary = props => {
                 <>
                     <div className="pluginCard">
                         <div className="pluginImage">
-                            <img src={fetchedPlugin.thumbnail} alt="Image du plugin" />
+                            <img src={fetchedPlugin.thumbnail} alt="Image du plugin"/>
                         </div>
                         <div className="pluginSideNotes">
                             <h1 className="title">{fetchedPlugin.name}</h1>
-                            <h3>Version : <Text code>{fetchedPlugin.version}</Text> </h3>
-                            <p>Category : {fetchedPlugin.category}</p>
-                            <p>Tags : {fetchedPlugin.tags.map((item) => {
-                                return <Tag>item</Tag>
+                            <p><span className="categoryName">{fetchedPlugin.category}</span></p>
+                            <p>{fetchedPlugin.tags.map((item) => {
+                                return (<Tag>{item}</Tag>)
                             })}</p>
 
-                            <Button type="primary" icon="download"><a className="downloadLink" href={`${BACKEND_ROOT_PATH}/submit/${fetchedPlugin._id}`}>Telecharger le plugin</a></Button>
-
-                            <div>
+                            <div className="likesCount">
                                 {fetchedPlugin.likes < 1 ?
-                                    <Icon className="cardIcons" style={{ fontSize: '30px' }} type="heart" theme="twoTone" twoToneColor="#eb2f96" onClick={handleLike} /> :
+                                    <Icon className="cardIcons" style={{fontSize: '30px'}} type="heart" theme="twoTone"
+                                          twoToneColor="#eb2f96" onClick={handleLike}/> :
                                     <svg width="30px" height="30px" fill="red" viewBox="0 0 1024 1024">
-                                        <path d="M923 283.6c-13.4-31.1-32.6-58.9-56.9-82.8-24.3-23.8-52.5-42.4-84-55.5-32.5-13.5-66.9-20.3-102.4-20.3-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5-24.4 23.9-43.5 51.7-56.9 82.8-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3 0.1-35.3-7-69.6-20.9-101.9z" />
+                                        <path
+                                            d="M923 283.6c-13.4-31.1-32.6-58.9-56.9-82.8-24.3-23.8-52.5-42.4-84-55.5-32.5-13.5-66.9-20.3-102.4-20.3-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5-24.4 23.9-43.5 51.7-56.9 82.8-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3 0.1-35.3-7-69.6-20.9-101.9z"/>
                                     </svg>
                                 }
                                 <label>{fetchedPlugin.likes}</label>
                             </div>
-
+                            <h3 className={"version"}>Version : <Text code>{fetchedPlugin.version}</Text></h3>
+                            <Button type="primary" icon="download"><a className="downloadLink"
+                                                                      href={`${BACKEND_ROOT_PATH}/submit/${fetchedPlugin._id}`}>Telecharger
+                                le plugin</a></Button>
                         </div>
                     </div>
 
@@ -87,15 +88,13 @@ const PluginSummary = props => {
                             <TabPane tab="Description" key="1">
                                 <div className="pluginDescription">
                                     <h1> Description </h1>
-
-                                    <Button type="primary" icon="play-circle">Essayer {fetchedPlugin.name}</Button>
-
                                     <p> Description du plugin séléctionné </p>
+                                    <Button type="primary" icon="play-circle">Essayer {fetchedPlugin.name}</Button>
                                 </div>
                             </TabPane>
                             <TabPane tab="Commentaires" key="2">
                                 <section className="commentSection">
-                                    <CommentComponent pluginId={fetchedPlugin._id} comments={fetchedPlugin.comments} />
+                                    <CommentComponent pluginId={fetchedPlugin._id} comments={fetchedPlugin.comments}/>
                                 </section>
                             </TabPane>
                         </Tabs>
--- Client/pawworkshop/src/component/core/navigation-bar/NavigationBar.js
+++ Client/pawworkshop/src/component/core/navigation-bar/NavigationBar.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { Menu, Icon, Button, Popover, Modal } from 'antd';
+import React, {useState} from 'react';
+import {Button, Icon, Menu, Modal, Popover} from 'antd';
 import './NavigationBar.css';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import WrappedSignInForm from './SignInForm';
 
 const popoverContent = () => {
@@ -10,7 +9,7 @@ const popoverContent = () => {
     //     <p>Random Customer</p></>)
     return ('<><img src="https://www.dictionary.com/e/wp-content/uploads/2018/03/PogChamp.jpg"></img> <p>Random Customer</p></>')
     // return (<p>Enfoiré</p>)
-}
+};
 
 const NavigationBar = props => {
 
@@ -18,42 +17,41 @@ const NavigationBar = props => {
 
     const hideModal = () => {
         setSignIn(false)
-    }
+    };
 
     const showSignIn = () => {
         setSignIn(true);
-    }
+    };
     return (
         <div className="navigationBar">
             <Menu
-                defaultSelectedKeys={['1']}
+                defaultSelectedKeys={['2']}
                 defaultOpenKeys={['sub1']}
                 mode="horizontal"
                 theme="dark"
                 className="navigationBarLinks"
             >
-                <Menu.Item key="1">
-                    <Icon type="pie-chart" />
-                    <span>Ajouter un plugin</span>
-
-                    <Link to="/form" />
-                </Menu.Item>
                 <Menu.Item key="2">
-                    <Icon type="desktop" />
+                    <Icon type="desktop"/>
                     <span>Visiter le magasin</span>
 
-                    <Link to="/workshop" />
+                    <Link to="/workshop"/>
 
                 </Menu.Item>
-                <Menu.Item key="3">
-                    <Icon type="inbox" />
-                    <span>Option 3</span>
+                <Menu.Item key="1">
+                    <Icon type="pie-chart"/>
+                    <span>Ajouter un plugin</span>
+
+                    <Link to="/form"/>
                 </Menu.Item>
             </Menu>
 
             <div className="navigationBarProfil" onClick={showSignIn}>
                 <p>Se connecter</p>
-                <Popover placement="bottomRight" content={<div className="popoverContent"><img className="popoverImageProfil" src="https://www.dictionary.com/e/wp-content/uploads/2018/03/PogChamp.jpg"></img> <p>Random Customer</p></div>} trigger="hover">
+                <Popover placement="bottomRight"
+                         content={<div className="popoverContent"><img className="popoverImageProfil"
+                                                                       src="https://www.dictionary.com/e/wp-content/uploads/2018/03/PogChamp.jpg"></img>
+                             <p>Random Customer</p></div>} trigger="hover">
                     <Button icon="user"></Button>
                 </Popover>
             </div>
@@ -67,15 +65,13 @@ const NavigationBar = props => {
                 className="signInModal"
                 footer={null}
             >
-                <WrappedSignInForm />
+                <WrappedSignInForm/>
             </Modal>
         </div>
 
     );
 };
 
-NavigationBar.propTypes = {
-
-};
+NavigationBar.propTypes = {};
 
 export default NavigationBar;
