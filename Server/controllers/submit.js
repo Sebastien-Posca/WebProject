e2ca979caec8bb44ffc6012c279170188917f0f7
Quentin <quentin.moreau@etu.unice.fr>
2020-02-24 17:46:30 +0100

    git

--- Server/controllers/submit.js
+++ Server/controllers/submit.js
@@ -5,12 +5,14 @@ var unzipper = require('unzipper')
 let pluginController = require('../controllers/plugin')
 const Plugin = require('../models/plugin')
 const simpleGit = require('simple-git/promise');
-
+const request = require('request');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     var dirname = './plugins/' + Date.now();
-    fs.mkdirSync(dirname, { recursive: true });
+    if (file != undefined) {
+      fs.mkdirSync(dirname, { recursive: true });
+    }
     cb(null, dirname);
   },
   filename: function (req, file, cb) {
@@ -73,38 +75,83 @@ exports.fileDownload = (req, res) => {
   });
 }
 
+let download = (url, dest) => {
+  return new Promise((resolve, reject) => {
+    let file = fs.createWriteStream(dest);
+    let stream = request({
+        /* Here you should specify the exact link to the file you are trying to download */
+        uri: url,
+        headers: {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+        },
+        /* GZIP true for most of the websites now, disable it if you don't need it */
+        gzip: true
+    }).pipe(file)
+    .on('finish', () => {
+        console.log(`The file is finished downloading.`);
+        resolve();
+    })
+    .on('error', (error) => {
+        reject(error);
+    })
+  });
+};
+
 exports.gitUpload = (req, res) => {
+  let gitArchive = req.body.git + "/archive/master.zip";
   let ts = Date.now()
   let dirname = __dirname + "/../plugins/" + ts;
+  let pluginFolder = ".";
   let main;
-  console.log(req.body.git);
+  console.log(ts);
   fsPromises.mkdir("./plugins/" + ts, {recursive: true})
   .then((err) => {
     if (err) throw err;
-    console.log("Starting to clone");
-    return simpleGit().clone(req.body.git, dirname)
+    console.log("Starting to download");
+    return download(gitArchive, dirname + "/file.zip")
   })
   .then(() => {
-    return fsPromises.access(dirname + '/main.json', fs.constants.F_OK)
+    return new Promise((resolve, reject) => {
+      let temp = fs.createReadStream(dirname + "/file.zip").pipe(unzipper.Extract({ path: dirname }));
+      temp.on("close", () => {
+        let arr = req.body.git.split("/");
+        pluginFolder = dirname + '/' + arr[arr.length - 1] + "-master";
+        console.log(pluginFolder);
+        resolve();
+      })
+    });
   })
   .then(() => {
-    return fsPromises.readFile(dirname + '/main.json');
+    return fsPromises.access(pluginFolder + '/main.json', fs.constants.F_OK);
+  })
+  .then(() => {
+    return fsPromises.readFile(pluginFolder + '/main.json');
   })
   .then((buffer) => {
     main = JSON.parse(buffer);
     console.log(main);
+    return fsPromises.rename(dirname + "/file.zip", pluginFolder + "/" + main.name + ".zip")
+  })
+  .then((err) => {
     if (req.body.thumbnail != undefined) {
-      return fsPromises.writeFile(dirname + '/thumbnail.jpg', req.body.thumbnail);
+      return fsPromises.writeFile(pluginFolder + '/thumbnail.jpg', req.body.thumbnail);
     } else {
-      return fsPromises.readFile(dirname + "/img/unknown.jpg", { encoding: 'base64' }).then((buff => {
-        return fsPromises.writeFile(dirname + '/thumbnail.jpg', buff);
+      return fsPromises.readFile(pluginFolder + "/img/unknown.jpg", { encoding: 'base64' }).then((buff => {
+        return fsPromises.writeFile(pluginFolder + '/thumbnail.jpg', buff);
       }));
     }
   })
   .then(() => {
-    let timestamp = dirname.split('./plugins/');
-    let path = "./plugin/" + timestamp[1];
-    return pluginController.createPlugin('moi', dirname, path, req.body.name, main.name, req.body.description, req.body.tags, req.body.categorie, req.body.version, req.body.git)
+    let pluggg = pluginFolder.split("/");
+    let path = "./plugin/" + ts + '/' + pluggg[pluggg.length - 1];
+    let loc = "./plugins/" + ts + '/' + pluggg[pluggg.length - 1];
+    return pluginController.createPlugin('moi', loc, path, req.body.name, main.name, req.body.description, req.body.tags, req.body.categorie, req.body.version, req.body.git)
   })
   .then((plugin) => {
     console.log("Upload request complete, ok");
